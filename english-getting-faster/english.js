163841239a83ef831646bfe3661b171d8b7eb5c5
Tirantbacon <dylanw2000@Bigpond.com>
2016-05-10 16:20:17 +1000

    Add files via upload

---
+++ english-getting-faster/english.js
@@ -0,0 +1,37 @@
+var english = new Howl({
+	src: ["audio/english.ogg"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 0.57686;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 1000;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.01;
+		english.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(0) + "%";
+	}
+
+	document.getElementById("lordenglish").style.top = (rate - Math.random() * rate * 2) + "px";
+	document.getElementById("lordenglish").style.left = (rate - Math.random() * rate * 2) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	english.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
