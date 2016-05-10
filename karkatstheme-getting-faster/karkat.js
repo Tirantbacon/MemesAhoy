2fe31cb1d2e20d140b24d5a0bc2d80982632d667
Tirantbacon <dylanw2000@Bigpond.com>
2016-05-10 16:04:41 +1000

    Add files via upload

---
+++ karkatstheme-getting-faster/karkat.js
@@ -0,0 +1,37 @@
+var karkat = new Howl({
+	src: ["audio/karkatstheme.ogg"],
+	loop: true,
+});
+
+var update_time = new Date();
+
+var percent_time = 0.57686;
+
+var rate = 1;
+var rate_timer = percent_time;
+
+function update() {
+
+	var new_time = new Date();
+	var delta = new_time.getTime() - update_time.getTime();
+	update_time.setTime(new_time.getTime());
+
+	rate_timer -= rate * delta / 1000;
+
+	if (rate_timer <= 0) {
+		rate_timer += percent_time;
+		rate += 0.01;
+		karkat.rate(rate);
+		document.getElementById("speed").innerHTML = "speed: " + (rate * 100).toFixed(0) + "%";
+	}
+
+	document.getElementById("karkat").style.top = (rate - Math.random() * rate * 2) + "px";
+	document.getElementById("karkat").style.left = (rate - Math.random() * rate * 2) + "px";
+	requestAnimationFrame(update);
+}
+
+function run() {
+	karkat.play();
+	update_time = new Date();
+	requestAnimationFrame(update);
+}
